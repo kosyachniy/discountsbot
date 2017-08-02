4e14b2d18b1816ffb6fe38d73b567a13388e04ca
Poloz Alexey <polozhev@mail.ru>
2017-08-02 22:40:49 +0300

    Pilot

---
+++ trend.py
@@ -0,0 +1,27 @@
+from func import *
+
+url='http://steamcommunity.com/market/search?q=#p'
+
+k=0
+su=0
+for s in range(10):
+	page=requests.get(url+str(s)+'_popular_desc').text
+	soup=BeautifulSoup(page, 'lxml')
+
+	table=soup.find('div', id='searchResultsRows')
+	a=table.find_all('a', class_='market_listing_row_link')
+
+	for i in a:
+		href=i.get('href')
+		print(href)
+		span=i.find('span', class_='normal_price')
+		normal=float(span.find('span', class_='normal_price').contents[0][1:-4])
+		sale=float(span.find('span', class_='sale_price').contents[0][1:-4])
+		print(normal, sale)
+		print('--------------------')
+
+		k+=1
+		su+=sale
+		with db:
+			db.execute("INSERT INTO note VALUES (%d, '%s', %f, 0)" % (k, href, normal))
+	time.sleep(1)
