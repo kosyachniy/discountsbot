b02fb1e3efadd13e6873e439cc353d27cd6cd379
Poloz Alexey <polozhev@mail.ru>
2017-08-03 18:27:09 +0300

    Description

--- create.py
+++
@@ -1,4 +0,0 @@
-from func import *
-
-with db:
-	db.execute("CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT, verified int, name text, surname text, sex int, birthday text, photo text, address text, language text, phone int, timezone int, home text, vkid int, vknick text, platform text, days int, genre text)")
