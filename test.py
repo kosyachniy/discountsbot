4dc324c69dc0b796b05cfa2b86be1e581b91aaa0
Poloz Alexey <polozhev@mail.ru>
2017-08-04 04:33:03 +0300

    Test 3

--- test.py
+++
@@ -1,8 +0,0 @@
-from func import *
-
-values = {'album_id': '247231511', 'group_id': '151313066'}
-url=vk.method('photos.getMessagesUploadServer', values)['upload_url']
-photo=open('1.jpg','rb')
-print(vk.http.post(url, files=photo))
-#response = json.loads(str())
-#vk.method('photos.save', response)
